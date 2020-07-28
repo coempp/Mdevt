bef86993f08bd78676181094369aed52482ffcf8
surajbohrar <51718792+surajbohrar@users.noreply.github.com>
2020-07-28 12:34:43 +0530

    v2

--- app/src/main/java/com/example/mdevt/signup.java
+++ app/src/main/java/com/example/mdevt/signup.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.Html;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
